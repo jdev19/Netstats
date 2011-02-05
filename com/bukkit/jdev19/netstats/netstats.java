00701c0e6a3c284385c15fc476de9059db7c6804
phyreman <j-m15@hotmail.com>
2011-02-04 19:53:15 -0800

    Added player handler

--- com/bukkit/jdev19/netstats/netstats.java
+++ com/bukkit/jdev19/netstats/netstats.java
@@ -2,15 +2,31 @@ package com.bukkit.jdev19.netstats;
 
 import java.io.File;
 import java.util.HashMap;
-import org.bukkit.entity.Player;
+
 import org.bukkit.Server;
-import org.bukkit.event.Event.Priority;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginLoader;
-
-
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.event.player.PlayerListener;
 
-public class netstats {
-
+public class netstats extends JavaPlugin {
+	private final NetPlayerListener playerListener = new NetPlayerListener(this);
+	
+	public netstats(PluginLoader pluginLoader, Server instance, PluginDescriptionFile desc, File folder, File plugin, ClassLoader cLoader) {
+		super(pluginLoader, instance, desc, folder, plugin, cLoader);
+	}
+	
+	@Override
+	public void onDisable() {
+		System.out.println("netstats Disabled");
+	}
+	
+	@Override
+	public void onEnable() {		
+		PluginDescriptionFile pdfFile = this.getDescription();
+		System.out.println(pdfFile.getName()+" version "+pdfFile.getVersion()+" is enabled.");
+	}
 }
