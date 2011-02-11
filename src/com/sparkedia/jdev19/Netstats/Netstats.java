57c7b58ae0274037827c13b5a7d9f2d6f290a0ac
phyreman <phyre19@gmail.com>
2011-02-10 18:07:47 -0800

    Fixed the .gitignore and how the files were being handled by Git

---
+++ src/com/sparkedia/jdev19/Netstats/Netstats.java
@@ -0,0 +1,37 @@
+package com.sparkedia.jdev19.Netstats;
+
+import java.io.File;
+
+import org.bukkit.Server;
+import org.bukkit.event.Event;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginLoader;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.PluginManager;
+
+public class Netstats extends JavaPlugin {
+	private final NetPlayerListener playerListener = new NetPlayerListener(this);
+	public static Property properties = new Property("plugins/Netstats/config.txt");
+	
+	public Netstats(PluginLoader pluginLoader, Server instance, PluginDescriptionFile desc, File folder, File plugin, ClassLoader cLoader) {
+		super(pluginLoader, instance, desc, folder, plugin, cLoader);
+	}
+	
+	@Override
+	public void onDisable() {
+		PluginDescriptionFile pdf = this.getDescription();
+		System.out.println("["+pdf.getName()+"] v"+pdf.getVersion()+" has been disabled.");
+	}
+	
+	@Override
+	public void onEnable() {
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvent(Event.Type.PLAYER_JOIN, this.playerListener, Event.Priority.Normal, this);
+		pm.registerEvent(Event.Type.PLAYER_QUIT, this.playerListener, Event.Priority.Normal, this);
+		pm.registerEvent(Event.Type.PLAYER_KICK, this.playerListener, Event.Priority.Normal, this);
+		pm.registerEvent(Event.Type.PLAYER_COMMAND, this.playerListener, Event.Priority.Normal, this);
+		
+		PluginDescriptionFile pdf = this.getDescription();
+		System.out.println("["+pdf.getName()+"] v"+pdf.getVersion()+" has been enabled.");
+	}
+}
