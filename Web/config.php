eef0c921720fe3bda4b6048a8a5e67b44c06f687
Josh Devecka <jdev17@live.com>
2011-02-14 16:46:01 -0800

    Just addin some stuffs.

--- Web/config.php
+++ Web/config.php
@@ -1,4 +1,9 @@
 <?php
+/* ----------------------------------------------
+File Created by Josh Devecka(Jdev19)
+You may use the code as you wish,
+but please give me some credit.
+------------------------------------------------- */
 $mysql_host = 'localhost';
 $mysql_user = 'root';
 $mysql_pass = 'PASSWORD';
